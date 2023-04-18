1a716caeda92ba7fe73800f0ce3b018802e8bcd8
jonas-45 <jonaswullah@gmail.com>
2023-04-18 13:13:31 +0000

    Rearrange constructor parameters

--- classes/person.rb
+++ classes/person.rb
@@ -1,7 +1,7 @@
 class Person
   attr_reader :id, :name, :age
 
-  def initialize(name = 'unknown',age,parent_permission = true)
+  def initialize(age, name = 'unknown', parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
