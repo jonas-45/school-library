aa7ba4caa9e36c204de6a3efd7259844dba2021f
jonas-45 <jonaswullah@gmail.com>
2023-04-18 13:05:53 +0000

    Add private of_age method an a public can_use_services methods

--- classes/person.rb
+++ classes/person.rb
@@ -1,31 +1,32 @@
 class Person
+  attr_reader :id, :name, :age
+
   def initialize(name = 'unknown',age,parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
-  end
-
-  def id
-    @id
+    @parent_permission = parent_permission
   end
 
   def id=(value)
     @id = value
   end
 
-  def name
-    @name
-  end
-
   def name=(value)
     @name = value
-  end
-
-  def age
-    @age
   end
 
   def age=(value)
     @age = value
   end
+
+  def can_use_services
+    of_age? || @parent_permission
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
 end
