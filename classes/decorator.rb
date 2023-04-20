f15624a007d18d01e41f627bc44eaa13a000be51
jonas-45 <jonaswullah@gmail.com>
2023-04-20 10:13:40 +0000

    Add CapitalDecorator class

--- classes/decorator.rb
+++ classes/decorator.rb
@@ -1,3 +1,19 @@
 class Decorator < Nameable
-  
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+#capitaldecorator class
+class CapitalizeDecorator < Decorator
+  def correct_name
+    super.capitalize()
+  end
 end
