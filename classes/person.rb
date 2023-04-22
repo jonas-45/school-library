8f1d284ba210aca71f37bae3facd8a74ad2c0291
jonas-45 <jonaswullah@gmail.com>
2023-04-22 21:21:37 +0000

    Add classroom and rental instance variables to app class

--- classes/person.rb
+++ classes/person.rb
@@ -5,20 +5,21 @@ class Person < Nameable
   attr_accessor :name, :age, :rentals
 
   def initialize(age, name = 'unknown', parent_permission: true)
-    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @rental = []
+    @rentals = []
+    super()
   end
 
   def can_use_services?
     of_age? || @parent_permission
   end
 
-  def add_rental(book, date)
-    Rental.new(date, book, self)
+  def add_rental(person, date = Date.today, book = self)
+    Rental.new(date, book, person)
+    @rentals << self unless @rentals.include?(self)
   end
 
   def correct_name
