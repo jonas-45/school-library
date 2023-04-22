650cc8ec0ff02ece5fb0b3231cf634b2af634115
jonas-45 <jonaswullah@gmail.com>
2023-04-22 20:07:19 +0000

    Add list_all_books method to app class

--- classes/app.rb
+++ classes/app.rb
@@ -1,6 +1,23 @@
+require_relative 'person'
+require_relative 'decorator'
+require_relative 'book'
+require_relative 'rental'
+require_relative 'teacher'
+require_relative 'student'
+require_relative 'classroom'
+
 class App
   def initialize
-
+    @books = []
   end
 
+  def list_all_books
+    if @books.empty?
+      puts 'There are no books in the library'
+    else
+      @books.each do |book|
+        puts "Title: #{book.title}, Author: #{book.author}".capitalize
+      end
+    end
+  end
 end
